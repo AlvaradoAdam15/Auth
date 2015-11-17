63f2847f2e06e379f21d95e52c5b84a911d8e8e9
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-11-17 21:10:10 +0100

    Add style

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,6 +5,10 @@
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet" type="text/css">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
     <style>
         html, body {
             height: 100%;
@@ -84,5 +88,8 @@
 
     </div>
 </div>
+
+<script src="{{ asset('js/all.js') }}"</script>
+<script src="{{ asset('js/main.js') }}"</script>
 </body>
 </html>
