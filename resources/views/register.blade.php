5a9c8a8ec9736778ebd93af782ec43300c5a5bf1
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-11-17 21:17:07 +0100

    change password_confirm to password_confirmation

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -72,7 +72,7 @@
                 <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <div class="form-group">
-                <label for="password_confirm">Confirm password</label>
+                <label for="password_confirmation">Confirm password</label>
                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
             </div>
 
